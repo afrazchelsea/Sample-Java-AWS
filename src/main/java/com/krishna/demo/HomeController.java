0c0e79e9fcf5d3d24ddda0a25c0b1dabaf43b9dd
Afraz Momin <41325430+afrazchelsea@users.noreply.github.com>
2020-11-09 16:49:20 +0530

    Updated Tests

--- src/main/java/com/krishna/demo/HomeController.java
+++ src/main/java/com/krishna/demo/HomeController.java
@@ -1,13 +1,7 @@
 package com.krishna.demo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,22 +12,18 @@ public class HomeController {
 	@RequestMapping("home")
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("home");
-
-		//mv.addObject("username", "");
-		
 		return mv;
 	}
 	
 	@RequestMapping("/")
 	public ModelAndView Default() {
 		ModelAndView mv = new ModelAndView("home");
-
-		//mv.addObject("username", "");
-		
 		return mv;
 	}
-
-
-
-
+	
+	@GetMapping("/home1")
+	public String home1() {
+		return "home1";
+	}
+	
 }
--- src/test/java/com/krishna/demo/MyWebAppApplicationTests.java
+++ src/test/java/com/krishna/demo/MyWebAppApplicationTests.java
@@ -1,13 +1,23 @@
 package com.krishna.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootTest
 class MyWebAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testHomeController() {
+		HomeController homeController = new HomeController();
+		String result = homeController.home1();
+		assertEquals(result, "home1");;
 	}
 
 }
